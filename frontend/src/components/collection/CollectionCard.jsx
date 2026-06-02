5d643530a097510f7b90a027c883a8333a0465ea
Raghav956 <119293256+Raghav956@users.noreply.github.com>
2026-06-02 22:14:44 +0530

    issues in collection page

--- frontend/src/components/collection/CollectionCard.jsx
+++ frontend/src/components/collection/CollectionCard.jsx
@@ -9,15 +9,32 @@ export default function CollectionCard({ collection }) {
       className="group block"
     >
 
-      <div className="overflow-hidden rounded-[32px]">
+     <div className="overflow-hidden rounded-[32px]">
 
-        <img
+  {collection.banner?.includes('.mp4') ||
+   collection.banner?.includes('/video/')
+    ? (
+      <video
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="h-[500px] w-full object-cover group-hover:scale-105 transition duration-700"
+      >
+        <source
           src={collection.banner}
-          alt={collection.title}
-          className="h-[500px] w-full object-cover group-hover:scale-105 transition duration-700"
+          type="video/mp4"
         />
+      </video>
+    ) : (
+      <img
+        src={collection.banner}
+        alt={collection.title}
+        className="h-[500px] w-full object-cover group-hover:scale-105 transition duration-700"
+      />
+    )}
 
-      </div>
+</div>
 
       <div className="mt-6">
 
